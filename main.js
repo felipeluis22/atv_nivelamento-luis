19dd4e50f65b03bfa2c0e94fa9e04dca2b7127bf
luis felipe <114739100+felipeluis22@users.noreply.github.com>
2024-03-21 08:46:29 -0300

    dd

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
-const contadores = document.querySelectorAll(".contador");
+
 for(let i=0; i <botoes.length; i++){
     botoes[i].onclick = function(){
         for(let j=0; j<botoes.length;j++){
@@ -12,5 +12,7 @@ for(let i=0; i <botoes.length; i++){
 
     }
 }
+const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-12-21T00:00:00");
-contadores[0].textContent = tempoObjetivo1;
+let tempoAtual = new Date();
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
